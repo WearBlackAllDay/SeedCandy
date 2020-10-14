7b8b63d465a398148f9cbe9e6edc5475b187fb0f
Lara <69314462+WearBlackAllDay@users.noreply.github.com>
2020-10-14 21:40:43 +0200

    update dependancy

--- src/main/java/gui/components/structurepanel/StructurePanel.java
+++ src/main/java/gui/components/structurepanel/StructurePanel.java
@@ -6,14 +6,12 @@ import gui.components.TextBlock;
 import kaptainwutax.biomeutils.source.OverworldBiomeSource;
 import kaptainwutax.seedutils.mc.MCVersion;
 import kaptainwutax.seedutils.mc.seed.StructureSeed;
+import swing.components.ButtonSet;
 import swing.components.GridPanel;
-import threading.ThreadPool;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
 
 public class StructurePanel extends JPanel {
 
@@ -25,58 +23,54 @@ public class StructurePanel extends JPanel {
         JPanel inputPanel = new JPanel(new BorderLayout());
         GridPanel<BiomeUnit> biomePanel = new GridPanel<>(1, 16, BiomeUnit::new);
         JPanel selectionPanel = new JPanel(new GridLayout(2, 2));
-        JButton longButton = new JButton("reverse to nextLong()");
-        JButton biomeButton = new JButton("crack with Biomes");
-        JButton verifyButton = new JButton("verify WorldSeeds");
-        JProgressBar progressBar = new JProgressBar();
+        ButtonSet<JButton> buttonSet = new ButtonSet<>(JButton::new,
+                "reverse to nextLong()", "crack with Biomes", "verify WorldSeeds");
+        JProgressBar progressBar = new JProgressBar(0,1);
 
-        progressBar.setMinimum(0);
         this.setLayout(new BorderLayout());
         this.setName("StructureSeed");
 
-        longButton.addActionListener(e -> {
-            outputText.setText("");
-            for (long seed : Strings.splitToLongs(this.inputText.getText())) {
-                StructureSeed.toRandomWorldSeeds(seed).forEach(worldseed -> outputText.addEntry(String.valueOf(worldseed)));
-            }
-        });
-
-        biomeButton.addActionListener(e -> {
-            outputText.setText("");
-            progressBar.setMaximum(Strings.countLines(this.inputText.getText()) * 65536);
-            AtomicInteger progress = new AtomicInteger(0);
-            SeedCandy.POOL.execute(Strings.splitToLongs(this.inputText.getText()), seed -> StructureSeed.getWorldSeeds(seed).forEachRemaining(candidate -> {
-                OverworldBiomeSource biomeSource = new OverworldBiomeSource(MCVersion.v1_16_2, candidate);
-                progressBar.setValue(progress.incrementAndGet());
-                boolean match = true;
-                for (int i = 0; i < 16; i++) {
-                    if (!biomePanel.componentAt(0, i).matches(biomeSource)) {
-                        match = false;
-                        break;
+        buttonSet.addListeners(
+                longButton -> {
+                    outputText.setText("");
+                    for (long seed : Strings.splitToLongs(this.inputText.getText())) {
+                        StructureSeed.toRandomWorldSeeds(seed).forEach(worldseed -> outputText.addEntry(String.valueOf(worldseed)));
+                    }
+                },
+                biomeButton -> {
+                    outputText.setText("");
+                    progressBar.setMaximum(Strings.countLines(this.inputText.getText()) * 65536);
+                    AtomicInteger progress = new AtomicInteger(0);
+                    SeedCandy.POOL.execute(Strings.splitToLongs(this.inputText.getText()), seed -> StructureSeed.getWorldSeeds(seed).forEachRemaining(candidate -> {
+                        OverworldBiomeSource biomeSource = new OverworldBiomeSource(MCVersion.v1_16_2, candidate);
+                        progressBar.setValue(progress.incrementAndGet());
+                        boolean match = true;
+                        for (int i = 0; i < 16; i++) {
+                            if (!biomePanel.componentAt(0, i).matches(biomeSource)) {
+                                match = false;
+                                break;
+                            }
+                        }
+                        if(match) SwingUtilities.invokeLater(() -> outputText.addEntry(String.valueOf(candidate)));
+                    }));
+                },
+                verifyButton -> {
+                    outputText.setText("");
+                    for (long seed : Strings.splitToLongs(this.inputText.getText())) {
+                        OverworldBiomeSource biomeSource = new OverworldBiomeSource(MCVersion.v1_16_2, seed);
+                        boolean match = true;
+                        for (int i = 0; i < 16; i++) {
+                            if (!biomePanel.componentAt(0, i).matches(biomeSource)) {
+                                match = false;
+                                break;
+                            }
+                        }
+                        if(match) outputText.addEntry(String.valueOf(seed));
                     }
                 }
-                if(match) SwingUtilities.invokeLater(() -> outputText.addEntry(String.valueOf(candidate)));
-            }));
-        });
+        );
 
-        verifyButton.addActionListener(e -> {
-            outputText.setText("");
-            for (long seed : Strings.splitToLongs(this.inputText.getText())) {
-                OverworldBiomeSource biomeSource = new OverworldBiomeSource(MCVersion.v1_16_2, seed);
-                boolean match = true;
-                for (int i = 0; i < 16; i++) {
-                    if (!biomePanel.componentAt(0, i).matches(biomeSource)) {
-                        match = false;
-                        break;
-                    }
-                }
-                if(match) outputText.addEntry(String.valueOf(seed));
-            }
-        });
-
-        selectionPanel.add(longButton);
-        selectionPanel.add(biomeButton);
-        selectionPanel.add(verifyButton);
+        buttonSet.addAll(selectionPanel);
         selectionPanel.add(progressBar);
         inputPanel.add(biomePanel, BorderLayout.CENTER);
         inputPanel.add(selectionPanel, BorderLayout.SOUTH);
--- src/main/java/gui/components/worldpanel/WorldPanel.java
+++ src/main/java/gui/components/worldpanel/WorldPanel.java
@@ -9,15 +9,13 @@ import kaptainwutax.seedutils.mc.pos.BPos;
 import kaptainwutax.seedutils.mc.seed.ChunkSeeds;
 import kaptainwutax.seedutils.mc.seed.WorldSeed;
 import swing.SwingUtils;
+import swing.components.ButtonSet;
 import swing.components.SelectionBox;
-import threading.ThreadPool;
 import util.QuadFinder;
 
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.stream.Collectors;
 
 public class WorldPanel extends JPanel{
     public WorldPanel() {
@@ -26,23 +24,18 @@ public class WorldPanel extends JPanel{
         JPanel inputPanel = new JPanel(new BorderLayout());
         JPanel buttonPanel = new JPanel(new GridLayout(0, 2));
         JPanel selectionPanel = new JPanel();
-        JButton nextLongButton = new JButton("check for nextLong()");
-        JButton quadButton = new JButton("locate Quadhuts");
-        JButton spawnButton = new JButton("show/filter Spawnpoint");
-        JButton hashButton = new JButton("convert to hash");
-        JButton shadowButton = new JButton("switch to ShadowSeed");
-        JButton sisterButton = new JButton("get SisterSeeds");
-        JButton structureButton = new JButton("reduce to StructureSeed");
-        JButton populationButton = new JButton("get PopulationSeed");
-        JButton regionButton = new JButton("get RegionSeed");
-        JButton pillarButton = new JButton("get PillarSeed");
-        JButton copyButton = new JButton("copy Output");
-        JButton clearButton = new JButton("clear text");
+        ButtonSet<JButton> buttonSet = new ButtonSet<>(JButton::new,
+                "check for nextLong()", "locate Quadhuts",
+                "show/filter Spawnpoint", "convert to hash",
+                "switch to ShadowSeed", "get SisterSeeds",
+                "reduce to StructureSeed", "get PopulationSeed",
+                "get RegionSeed", "get PillarSeed",
+                "copy Output", "clear text");
         JTextField xCord = new JTextField();
         JTextField zCord = new JTextField();
         JTextField saltField = new JTextField();
         SelectionBox<MCVersion> versionSelector = new SelectionBox<>(MCVersion.v1_16, MCVersion.v1_15, MCVersion.v1_14, MCVersion.v1_13);
-        JProgressBar progressBar = new JProgressBar();
+        JProgressBar progressBar = new JProgressBar(0,1);
 
         this.setLayout(new BorderLayout());
         this.setName("WorldSeed");
@@ -53,130 +46,110 @@ public class WorldPanel extends JPanel{
         SwingUtils.setPrompt("Z", zCord);
         saltField.setPreferredSize(new Dimension(60, 25));
         SwingUtils.setPrompt("salt", saltField);
-        progressBar.setMinimum(0);
 
-        nextLongButton.addActionListener(e -> {
-            outputText.setText("");
-            for (long seed : Strings.splitToLongs(inputText.getText())) {
-                outputText.addEntry(WorldSeed.isRandom(seed) ? "possible nextLong()" : "is NOT nextLong()");
-            }
-        });
-
-        quadButton.addActionListener(e -> {
-            outputText.setText("");
-            progressBar.setMaximum(Strings.countLines(inputText.getText()));
-            SeedCandy.POOL.execute((Strings.splitToLongs(inputText.getText())), seed -> outputText.addEntry(QuadFinder.find(seed, versionSelector.getSelected())));
-        });
-
-        spawnButton.addActionListener(e -> {
-            MCVersion version = versionSelector.getSelected();
-            outputText.setText("");
-            try {
-                BPos search = new BPos(Integer.parseInt(xCord.getText().trim()),0, Integer.parseInt(zCord.getText().trim()));
-
-                SeedCandy.POOL.execute(Strings.splitToLongs(inputText.getText()), seed -> {
-                    OverworldBiomeSource biomeSource = new OverworldBiomeSource(version, seed);
-                    if (biomeSource.getSpawnPoint().equals(search)) {
-                        outputText.addEntry(String.valueOf(seed));
+        buttonSet.addListeners(
+                nextLongButton -> {
+                    outputText.setText("");
+                    for (long seed : Strings.splitToLongs(inputText.getText())) {
+                        outputText.addEntry(WorldSeed.isRandom(seed) ? "possible nextLong()" : "is NOT nextLong()");
                     }
-                });
-            } catch (NumberFormatException exception) {
-                SeedCandy.POOL.execute(Strings.splitToLongs(inputText.getText()), seed -> {
-                    OverworldBiomeSource biomeSource = new OverworldBiomeSource(version, seed);
-                        outputText.addEntry(String.format("%d (%d, %d)", seed, biomeSource.getSpawnPoint().getX(), biomeSource.getSpawnPoint().getZ()));
-                });
+                },
+                quadButton -> {
+                    outputText.setText("");
+                    progressBar.setMaximum(Strings.countLines(inputText.getText()));
+                    SeedCandy.POOL.execute((Strings.splitToLongs(inputText.getText())), seed ->
+                            outputText.addEntry(QuadFinder.find(seed, versionSelector.getSelected())));
+                },
+                spawnButton -> {
+                    MCVersion version = versionSelector.getSelected();
+                    outputText.setText("");
+                    try {
+                        BPos search = new BPos(Integer.parseInt(xCord.getText().trim()),0, Integer.parseInt(zCord.getText().trim()));
 
-            }
-        });
+                        SeedCandy.POOL.execute(Strings.splitToLongs(inputText.getText()), seed -> {
+                            OverworldBiomeSource biomeSource = new OverworldBiomeSource(version, seed);
+                            if (biomeSource.getSpawnPoint().equals(search)) {
+                                outputText.addEntry(String.valueOf(seed));
+                            }
+                        });
+                    } catch (NumberFormatException exception) {
+                        SeedCandy.POOL.execute(Strings.splitToLongs(inputText.getText()), seed -> {
+                            OverworldBiomeSource biomeSource = new OverworldBiomeSource(version, seed);
+                            outputText.addEntry(String.format("%d (%d, %d)", seed, biomeSource.getSpawnPoint().getX(), biomeSource.getSpawnPoint().getZ()));
+                        });
 
-        hashButton.addActionListener(e -> {
-            outputText.setText("");
-            for (long seed : Strings.splitToLongs(inputText.getText())) {
-                outputText.addEntry(String.valueOf(WorldSeed.toHash(seed)));
-            }
-        });
+                    }
+                },
+                hashButton -> {
+                    outputText.setText("");
+                    for (long seed : Strings.splitToLongs(inputText.getText())) {
+                        outputText.addEntry(String.valueOf(WorldSeed.toHash(seed)));
+                    }
+                },
+                shadowButton -> {
+                    outputText.setText("");
+                    for (long seed : Strings.splitToLongs(inputText.getText())) {
+                        outputText.addEntry(String.valueOf(WorldSeed.getShadowSeed(seed)));
+                    }
+                },
+                sisterButton -> {
+                    outputText.setText("");
+                    ArrayList<Long> seedList = new ArrayList<>(Strings.splitToLongs(inputText.getText()).length * 65536);
+                    for (long seed : Strings.splitToLongs(inputText.getText())) {
+                        WorldSeed.getSisterSeeds(seed).forEachRemaining(seedList::add);
+                    }
+                    outputText.setText(String.join("\n", seedList.stream().map(String::valueOf).toArray(String[]::new)));
+                },
+                structureButton -> {
+                    outputText.setText("");
+                    for (long seed : Strings.splitToLongs(inputText.getText())) {
+                        outputText.addEntry(String.valueOf(WorldSeed.toStructureSeed(seed)));
+                    }
+                },
+                populationButton -> {
+                    outputText.setText("");
+                    MCVersion version = versionSelector.getSelected();
+                    int x,z;
+                    try {
+                        x = Integer.parseInt(xCord.getText().trim());
+                        z = Integer.parseInt(zCord.getText().trim());
+                    } catch (NumberFormatException exception) {
+                        x = z = 0;
+                    }
+                    for (long seed : Strings.splitToLongs(inputText.getText())) {
+                        outputText.addEntry(String.valueOf(ChunkSeeds.getPopulationSeed(seed, x, z, version)));
+                    }
+                },
+                regionButton -> {
+                    outputText.setText("");
+                    MCVersion version = versionSelector.getSelected();
+                    int x, z, salt;
+                    try {
+                        x = Integer.parseInt(xCord.getText().trim());
+                        z = Integer.parseInt(zCord.getText().trim());
+                        salt = Integer.parseInt(saltField.getText().trim());
+                    } catch (NumberFormatException exception) {
+                        x = z = 0;
+                        salt = 14357620;
+                    }
+                    for (long seed : Strings.splitToLongs(inputText.getText())) {
+                        outputText.addEntry(String.valueOf(ChunkSeeds.getRegionSeed(seed, x, z, salt, version)));
+                    }
+                },
+                pillarButton -> {
+                    outputText.setText("");
+                    for (long seed : Strings.splitToLongs(inputText.getText())) {
+                        outputText.addEntry(String.valueOf(WorldSeed.toPillarSeed(seed)));
+                    }
+                },
+                copyButton -> Strings.clipboard(outputText.getText()),
+                clearButton -> {
+                    inputText.setText("");
+                    outputText.setText("");
+                }
+        );
 
-        shadowButton.addActionListener(e -> {
-            outputText.setText("");
-            for (long seed : Strings.splitToLongs(inputText.getText())) {
-                outputText.addEntry(String.valueOf(WorldSeed.getShadowSeed(seed)));
-            }
-        });
-
-        sisterButton.addActionListener(e -> {
-            outputText.setText("");
-            ArrayList<Long> seedList = new ArrayList<>(Strings.splitToLongs(inputText.getText()).length * 65536);
-            for (long seed : Strings.splitToLongs(inputText.getText())) {
-                WorldSeed.getSisterSeeds(seed).forEachRemaining(seedList::add);
-            }
-            outputText.setText(String.join("\n", seedList.stream().map(String::valueOf).toArray(String[]::new)));
-        });
-
-        structureButton.addActionListener(e -> {
-            outputText.setText("");
-            for (long seed : Strings.splitToLongs(inputText.getText())) {
-                outputText.addEntry(String.valueOf(WorldSeed.toStructureSeed(seed)));
-            }
-        });
-
-        populationButton.addActionListener(e -> {
-            outputText.setText("");
-            MCVersion version = versionSelector.getSelected();
-            int x,z;
-            try {
-                x = Integer.parseInt(xCord.getText().trim());
-                z = Integer.parseInt(zCord.getText().trim());
-            } catch (NumberFormatException exception) {
-                x = z = 0;
-            }
-            for (long seed : Strings.splitToLongs(inputText.getText())) {
-                outputText.addEntry(String.valueOf(ChunkSeeds.getPopulationSeed(seed, x, z, version)));
-            }
-        });
-
-        regionButton.addActionListener(e -> {
-            outputText.setText("");
-            MCVersion version = versionSelector.getSelected();
-            int x, z, salt;
-            try {
-                x = Integer.parseInt(xCord.getText().trim());
-                z = Integer.parseInt(zCord.getText().trim());
-                salt = Integer.parseInt(saltField.getText().trim());
-            } catch (NumberFormatException exception) {
-                x = z = 0;
-                salt = 14357620;
-            }
-            for (long seed : Strings.splitToLongs(inputText.getText())) {
-                outputText.addEntry(String.valueOf(ChunkSeeds.getRegionSeed(seed, x, z, salt, version)));
-            }
-        });
-
-        pillarButton.addActionListener(e -> {
-            outputText.setText("");
-            for (long seed : Strings.splitToLongs(inputText.getText())) {
-                outputText.addEntry(String.valueOf(WorldSeed.toPillarSeed(seed)));
-            }
-        });
-
-        copyButton.addActionListener(e -> Strings.clipboard(outputText.getText()));
-
-        clearButton.addActionListener(e -> {
-            inputText.setText("");
-            outputText.setText("");
-        });
-
-        buttonPanel.add(nextLongButton);
-        buttonPanel.add(quadButton);
-        buttonPanel.add(spawnButton);
-        buttonPanel.add(hashButton);
-        buttonPanel.add(shadowButton);
-        buttonPanel.add(sisterButton);
-        buttonPanel.add(structureButton);
-        buttonPanel.add(populationButton);
-        buttonPanel.add(regionButton);
-        buttonPanel.add(pillarButton);
-        buttonPanel.add(copyButton);
-        buttonPanel.add(clearButton);
+        buttonSet.addAll(buttonPanel);
         selectionPanel.add(xCord);
         selectionPanel.add(zCord);
         selectionPanel.add(saltField);
