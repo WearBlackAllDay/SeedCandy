3c215b07d271bc8e5174e14d6ae4ff9c2fc3b5a7
Lara <69314462+WearBlackAllDay@users.noreply.github.com>
2022-02-27 17:14:13 +0100

    update config

--- src/main/java/wearblackallday/seedcandy/util/Config.java
+++ src/main/java/wearblackallday/seedcandy/util/Config.java
@@ -3,43 +3,23 @@ package wearblackallday.seedcandy.util;
 import com.seedfinding.mccore.version.MCVersion;
 import wearblackallday.seedcandy.SeedCandy;
 
-import java.io.*;
-import java.net.URISyntaxException;
-import java.util.Properties;
+import java.util.prefs.Preferences;
 
 public class Config {
-	private static final File FILE;
-	private static final Properties CONFIG = new Properties();
-
-	static {
-		try {
-			FILE = new File(Config.class.getResource("/config.properties").toURI());
-			load();
-		} catch(URISyntaxException | IOException e) {
-			throw new RuntimeException();
-		}
-	}
-
-	public static void load() throws IOException{
-		CONFIG.load(new FileInputStream(FILE));
-	}
+	private static final Preferences ROOT = Preferences.userRoot().node("seedcandy");
 
 	public static void save() {
-		CONFIG.setProperty("version", SeedCandy.get().version.name);
-		CONFIG.setProperty("theme", SeedCandy.get().theme.getName());
-		try {
-			CONFIG.store(new FileOutputStream(FILE), null);
-		} catch(IOException ignored) {
-		}
+		ROOT.put("theme", SeedCandy.get().theme.getName());
+		ROOT.put("mcversion", SeedCandy.get().version.name);
 	}
 
 	public static MCVersion version() {
-		return MCVersion.fromString(CONFIG.getProperty("version"));
+		return MCVersion.fromString(ROOT.get("mcversion", "1.17"));
 	}
 
 	public static Class<?> theme() {
 		try {
-			return Class.forName(CONFIG.getProperty("theme"));
+			return Class.forName(ROOT.get("theme", "com.formdev.flatlaf.intellijthemes.FlatOneDarkIJTheme"));
 		} catch(ClassNotFoundException e) {
 			throw new RuntimeException();
 		}
